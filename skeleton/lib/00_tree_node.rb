288ef80f00286589d56f8388668305990e6541c1
Eric Balfour <egbalfour@gmail.com>
2022-04-15 14:04:31 -0400

    add initialize and parent setter

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
 
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children =[]
+    end
+
+    def parent=(node = nil)
+        parent.children.delete(self) if parent
+        @parent = node 
+        parent.children << self if parent 
+    end
+    
 end
